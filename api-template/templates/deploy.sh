e43d87b8b02c5cf73e2ba171d9e93f1b23fe5cf7
Maurício Gomes <alcides.filho@orangestack.com>
2022-06-30 10:58:53 -0300

    Update deploy

--- api-template/templates/deploy.sh
+++ api-template/templates/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash    
 curl --location --request PUT 'https://runtime-engine-api.runtimes.stackspot.com/apps/{{global_inputs.app_runtime_id}}/deploys/' \
 --header 'Content-Type: multipart/form-data' \
---form 'spec=@"{{target_path}}/oam.yaml"' \
+--form 'spec=@"./oam.yaml"' \
 --form 'target="7adc2243-aefe-4f8a-9298-7ecd27654b38"' 
